a9f0a14aa5e7e5a1387daf717505faddd4685726
Pranjal Gupta <pranjalg131@gmail.com>
2021-06-14 04:47:40 +0000

    Reorganised the component files and switched to arrow function declarations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -23,6 +23,6 @@ function App() {
     },
   ];
   return <Expenses items={expenses}></Expenses>;
-}
+};
 
 export default App;
