7a9f286b5c5f671db673cf93a61475132203edc0
Pranjal Gupta <pranjalg131@gmail.com>
2021-06-15 06:25:57 +0000

    Added Expenses filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from 'react';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = (props) => {
   const { items } = props;
 
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  console.log(filteredYear);
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filteredYear}
+        />
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
